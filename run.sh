4e628be494e96e88defc8079e08d425fc0aa903b
Kubzelll <kubzel@kubzel.dev>
2024-01-26 07:37:36 -0500

    Changed run.sh script to stop on error and added function to only compile the os not to run it

--- run.sh
+++ run.sh
@@ -1,20 +1,54 @@
-#assemble boot.s file
-as --32 boot.s -o boot.o
+#!/bin/bash
 
-#compile kernel.c file
-gcc -m32 -c kernel/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+build_kernel() {
+    # Assemble boot.s file
+    as --32 boot.s -o boot.o
+    if [ $? -ne 0 ]; then
+        echo "Error assembling boot.s. Exiting."
+        exit 1
+    fi
 
-#linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T linker.ld kernel.o boot.o -o MyOS.bin
+    gcc -m32 -c kernel/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+    if [ $? -ne 0 ]; then
+        echo "Error compiling kernel.c. Exiting."
+        exit 1
+    fi
+    ld -m elf_i386 -T linker.ld kernel.o boot.o -o MyOS.bin
+    if [ $? -ne 0 ]; then
+        echo "Error linking kernel. Exiting."
+        exit 1
+    fi
 
-#check MyOS.bin file is x86 multiboot file or not
-grub-file --is-x86-multiboot MyOS.bin
+    mkdir -p isodir/boot/grub
+    cp MyOS.bin isodir/boot/MyOS.bin
+    cp grub.cfg isodir/boot/grub/grub.cfg
+    grub-mkrescue -o MyOS.iso isodir
+    if [ $? -ne 0 ]; then
+        echo "Error building ISO. Exiting."
+        exit 1
+    fi
 
-#building the iso file
-mkdir -p isodir/boot/grub
-cp MyOS.bin isodir/boot/MyOS.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o MyOS.iso isodir
+    echo "Os built successfully."
+}
 
-#run it in qemu
-qemu-system-x86_64 -cdrom MyOS.iso
+echo "Do you want to (1) build kernel only or (2) build and run?"
+read choice
+
+case $choice in
+    1)
+        build_kernel
+        ;;
+
+    2)
+        build_kernel
+        qemu-system-x86_64 -cdrom MyOS.iso
+        exit $?
+        ;;
+
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
+
+exit $?
